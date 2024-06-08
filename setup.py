2cdb68bcec177bef26acfb9604ebf3cf003f6abc
sushanth-0 <102034708+sushanth-0@users.noreply.github.com>
2024-06-08 07:12:02 -0400

    Project Setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="customer_churn",
+    version="0.0.0",
+    author="Sushanth",
+    author_email="racham.sushanth@gmail.com",
+    packages=find_packages()
+)

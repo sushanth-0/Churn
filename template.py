3f8b47d7c1b2e30350f9ef289bf427c7a5e03fe5
sushanth-0 <102034708+sushanth-0@users.noreply.github.com>
2024-06-08 08:33:51 -0400

    mongoDB

--- template.py
+++ template.py
@@ -44,7 +44,7 @@ for filepath in list_of_files:
     if filedir != "":
         os.makedirs(filedir, exist_ok=True)
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
-        with open(filepath, "w") as f:
+        with open(filepath, "w", encoding="utf-8") as f:
             pass
     else:
         print(f"file is already present at: {filepath}")
